07d1bb84a6ca2a3068e0fa8ab89fda6b8c4c4588
RitiKSIBJr <letter.ritik011@gmail.com>
2024-01-03 15:45:03 +0545

    filter

--- pkg/scraper/scraper.py
+++ pkg/scraper/scraper.py
@@ -5,13 +5,73 @@ import json
 
 def connect_daraz():
     try:
-        response = requests.get('https://www.daraz.com.np/catalog/?q=snacks')
+        response = requests.get('https://www.daraz.com.np/catalog/?q=snacks', headers = {'Accept': 'application/json'})
 
     except Exception as error:
         raise(error)
-    
-    return response
 
+    return response.text
+
+
+def extract_data(res):
+    html_lines = res.split('window.pageData=')[1].split('</script')[0]
+
+    products = json.loads(html_lines)['mods']['listItems']
+    results = []
+
+    for product in products:
+        name = product['name']
+        price = product['utLogMap']['originalPrice']
+        product_url = product['productUrl']
+
+        quantity = find_quantity(name)
+        
+        results.append({'name':name, 'price':price, 'product_url':product_url, 'quantity': quantity})
+
+    results = filter_qunatity(results)
+    sort_asec(results)
+    return results
+
+
+def find_quantity(name):
+    word_list = name.split(' ')
+    last_word = word_list[-1]
+
+    if any(m in last_word for m in ['gm', 'g', 'kg', 'Gm', 'G']):
+        if any(n.isdigit() for n in last_word ):
+            return last_word
+        else:
+            if any(n.isdigit() for n in word_list[-2]):
+                return word_list[-2] + word_list[-1]
+            
+def filter_qunatity(results):
+    for result in results:
+        if result['quantity'] != None:
+            if '+' in result['quantity']:
+                result['quantity'] = result['quantity'].split('+')[0]
+            elif '-' in result['quantity']:
+                result['quantity'] = result['quantity'].split('-')[-1].replace('-','')
+
+            if 'kg' in result['quantity'] or 'Kg' in result['quantity']:
+                result['quantity'] = int(result['quantity'].replace('kg', '000').replace('Kg', '000')) 
+            else:
+                result['quantity'] = result['quantity'].replace('gm', '').replace('Gm', '').replace('g', '').replace('G', '').replace('(', '').replace(')','')
+                result['quantity'] = int(result['quantity'])
+        else:
+            result['quantity'] = 0
+            
+    return results
+
+def sort_asec(results):
+    sorted_result_asec = sorted(results, key=lambda x: x.get('quantity', float('inf')))
+    return sorted_result_asec
+
+    
+
+
+def sort_dsec(results):
+    sorted_result_desc = sorted(results, reverse=True, key=lambda x: x.get('quantity', float('inf')))
+    return sorted_result_desc
 
 
 def get_product_detail():
@@ -42,4 +102,4 @@ def get_product_detail():
        raise error
 
 if __name__ == "__main__":
-    connect_daraz()
+    extract_data(res=connect_daraz())
